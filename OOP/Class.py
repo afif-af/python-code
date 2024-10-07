e1cca73edd0e0f6da5115143e78e2147b73a3e64
afifaf13 <mdafifafrozfahim@gmail.com>
2024-10-07 15:35:40 +0600

    s

--- OOP/Class.py
+++ OOP/Class.py
@@ -108,6 +108,35 @@ my_used_car.increment_odometer(100)
 my_used_car.read_odometer()
 
 
+#inheritance
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def update_odometer(self,mileage):
+       if mileage>=self.odometer_reading:
+           self.odometer_reading=mileage
+       else:
+           print("you canit roll back an odomete! ")
+
+    def increment_odometer(self,miles):
+        self.odometer_reading+=miles
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+
+        super().__init__(make, model, year)
+
+my_leaf=ElectricCar('nissan','leaf',2024)
+print(my_leaf.get_descriptive_name())
 
 
 
