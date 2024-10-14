7d3a2537d5183da757bc2461966f79b918ece7bb
afifaf13 <mdafifafrozfahim@gmail.com>
2024-10-14 12:02:07 +0600

    d

--- Web Application/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Topic(models.Model):
-    text = models.ChatField(max_length=200)
-    data_added_models.DataTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.text
